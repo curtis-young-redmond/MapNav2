812b83b054f779764a632b2b9b5915f4afe3dcce
curtis-young-redmond <39505350+curtis-young-redmond@users.noreply.github.com>
2018-05-21 21:52:43 -0700

    Map Navigation
    Test project for iTrellis

---
+++ MapNav/MapNav/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace MapNav
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBox1.Location = new System.Drawing.Point(12, 44);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 169);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 270);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(300, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start navigation walk";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoEllipsis = true;
+            this.label1.BackColor = System.Drawing.Color.Silver;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 32);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Navigation steps";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 311);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Number of Blocks from drop point";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(212, 308);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(31, 17);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(68, 19);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(31, 17);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(232, 19);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(48, 17);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "clear";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 219);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(300, 43);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sample Data";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 338);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Map Navigation";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ MapNav/MapNav/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MapNav
+{
+    public partial class Form1 : Form
+    {
+        #region Local Variables
+        enum direction
+        {
+            posy,
+            negy,
+            posx,
+            negx
+        }
+        Coord mycoord;
+        direction currentstate = direction.posy; //due north initial state
+        #endregion
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                mycoord = new Coord();
+                char[] splitchar = { ',', ' ', ';', ':' };
+                var mywalkinstruction = textBox1.Text.Split(splitchar, StringSplitOptions.RemoveEmptyEntries);
+                var arrsize = mywalkinstruction.Count();
+                System.Drawing.Point[] myptarry = new Point[arrsize + 1];
+                myptarry[0] = new Point(0, 0);
+                int i = 1;
+                foreach (var step in mywalkinstruction)
+                {
+                    var vector = step[0];
+                    var steps = step.Substring(1).ToString();
+                    var numOfSteps = Convert.ToInt32(steps.ToString());
+                    StateTransition(vector, numOfSteps);
+                    myptarry[i++] = mycoord.mypoint;
+                }
+                textBox3.Text = mycoord.blocksFromDrop().ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+        }
+        void StateTransition(Char c, int numsteps)
+        {
+            switch (currentstate)
+            {
+                case direction.posy:
+                    {
+                        if (char.ToUpper(c) == 'R')
+                        {
+                            currentstate = direction.posx;
+                            mycoord.updateXposition(numsteps);
+                        }
+                        if (char.ToUpper(c) == 'L')
+                        {
+                            currentstate = direction.negx;
+                            mycoord.updateXposition(-numsteps);
+                        }
+                    }
+                    break;
+                case direction.posx:
+                    {
+                        if (char.ToUpper(c) == 'R')
+                        {
+                            currentstate = direction.negy;
+                            mycoord.updateYposition(-numsteps);
+                        }
+                        if (char.ToUpper(c) == 'L')
+                        {
+                            currentstate = direction.posy;
+                            mycoord.updateYposition(numsteps);
+                        }
+                    }
+                    break;
+                case direction.negx:
+                    {
+                        if (char.ToUpper(c) == 'R')
+                        {
+                            currentstate = direction.posy;
+                            mycoord.updateYposition(numsteps);
+                        }
+                        if (char.ToUpper(c) == 'L')
+                        {
+                            currentstate = direction.negy;
+                            mycoord.updateYposition(-numsteps);
+                        }
+                    }
+                    break;
+                case direction.negy:
+                    {
+                        if (char.ToUpper(c) == 'R')
+                        {
+                            currentstate = direction.negx;
+                            mycoord.updateXposition(-numsteps);
+                        }
+                        if (char.ToUpper(c) == 'L')
+                        {
+                            currentstate = direction.posx;
+                            mycoord.updateXposition(numsteps);
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        #region TEST DATA
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "L3, R2, L5, R1, L1, L2";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "L3, R2, L5, R1, L1, L2, L2, R1, R5, R1, L1, L2, R2, R4, L4, L3, L3, R5, L1, R3, L5, L2, R4, L5, R4, R2, L2, L1, R1, L3, L3, R2, R1, L4, L1, L1, R4, R5, R1, L2, L1, R188, R4, L3, R54, L4, R4, R74, R2, L4, R185, R1, R3, R5, L2, L3, R1, L1, L3, R3, R2, L3, L4, R1, L3, L5, L2, R2, L1, R2, R1, L4, R5, R4, L5, L5, L4, R5, R4, L5, L3, R4, R1, L5, L4, L3, R5, L5, L2, L4, R4, R4, R2, L1, L3, L2, R5, R4, L5, R1, R2, R5, L2, R4, R5, L2, L3, R3, L4, R3, L2, R1, R4, L5, R1, L5, L3, R4, L2, L2, L5, L5, R5, R2, L5, R1, L3, L2, L2, R3, L3, L4, R2, R3, L1, R2, L5, L3, R4, L4, R4, R3, L3, R1, L3, R5, L5, R1, R5, R3, L1";
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+        #endregion
+
+    }
+
+    #region Supporting Class
+    public class Coord
+    {
+        public Point mypoint;
+        public int maxblocks;
+        public Coord()
+        {
+            mypoint = new Point();
+            maxblocks = 0;
+        }
+        public void updateXposition(int x)
+        {
+            mypoint.X += x;
+        }
+        public void updateYposition(int y)
+        {
+            mypoint.Y += y;
+        }
+        public int blocksFromDrop()
+        {
+            return Math.Abs(mypoint.X) + Math.Abs(mypoint.Y);
+        }
+    }
+    #endregion
+}
